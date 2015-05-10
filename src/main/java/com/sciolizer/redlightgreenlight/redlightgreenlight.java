51d8f3f71b9ff39c00ed85ab6c837fea4dd1d27e
Joshua Ball <joshball@yahoo-inc.com>
2015-05-09 22:23:53 -0700

    tracking all the events

--- src/main/java/com/sciolizer/redlightgreenlight/redlightgreenlight.java
+++ src/main/java/com/sciolizer/redlightgreenlight/redlightgreenlight.java
@@ -5,10 +5,16 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.HumanEntity;
-import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.*;
+import org.bukkit.event.player.*;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
+import org.bukkit.event.vehicle.VehicleEvent;
+import org.bukkit.event.vehicle.VehicleExitEvent;
+import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -17,62 +23,234 @@ import java.util.WeakHashMap;
 
 public class RedLightGreenLight extends JavaPlugin {
 
-    protected WeakHashMap<Entity, Location> spawnLocations = new WeakHashMap<Entity, Location>();
-    protected WeakHashMap<Player,Location> previousPlayerLocations = new WeakHashMap<Player, Location>();
+    protected WeakHashMap<Entity, Location> entityLocations = new WeakHashMap<Entity, Location>();
+    protected boolean atLeastOnePlayerActedLastTick = false;
+    protected boolean atLeastOnePlayerActedThisTick = false;
 
-	public void onDisable() {
-	}
+    public void onDisable() {
+    }
 
-	public void onEnable() {
-		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvents(new Listener() {
+    public void onEnable() {
+        PluginManager pm = this.getServer().getPluginManager();
+        pm.registerEvents(new Listener() {
             @EventHandler
             public void onBlockPlace(BlockPlaceEvent event) {
                 Bukkit.getServer().broadcastMessage(event.getBlock().getType() + " placed at " + event.getBlock().getLocation() + "by player " + event.getPlayer().getName());
             }
         }, this);
 
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerBedEnterEvent(PlayerBedEnterEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerBedLeaveEvent(PlayerBedLeaveEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerBucketEmptyEvent(PlayerBucketEmptyEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerBucketFillEvent(PlayerBucketFillEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerDropItemEvent(PlayerDropItemEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerEditBookEvent(PlayerEditBookEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerEggThrowEvent(PlayerEggThrowEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerFishEvent(PlayerFishEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerInteractEvent(PlayerInteractEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerItemConsumeEvent(PlayerItemConsumeEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerItemHeldEvent(PlayerItemHeldEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerMoveEvent(PlayerMoveEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerPickupItemEvent(PlayerPickupItemEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerPortalEvent(PlayerPortalEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerShearEntityEvent(PlayerShearEntityEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerLeashEntityEvent(PlayerLeashEntityEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onPlayerUnleashEntityEvent(PlayerUnleashEntityEvent event) {
+                setPlayerActed();
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onVehicleEnterEvent(VehicleEnterEvent event) {
+                processVehicleEvent(event);
+            }
+
+            @EventHandler
+            public void onVehicleMoveEvent(VehicleMoveEvent event) {
+                processVehicleEvent(event);
+            }
+
+            @EventHandler
+            public void onVehicleExitEvent(VehicleExitEvent event) {
+                processVehicleEvent(event);
+            }
+        }, this);
+
+        pm.registerEvents(new Listener() {
+            @EventHandler
+            public void onEntityBreakDoorEvent(EntityBreakDoorEvent event) {
+                processEntityEvent(event);
+            }
+
+            @EventHandler
+            public void onEntityCreateProtalEvent(EntityCreatePortalEvent event) {
+                processEntityEvent(event);
+            }
+
+            @EventHandler
+            public void onEntityCombustByEntityEvent(EntityCombustByEntityEvent event) {
+                processEntityEvent(event); //  (unsure if self?)
+            }
+
+            @EventHandler
+            public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
+                processEntityEvent(event); // (towards other? or duplicates attack event?)
+            }
+
+            @EventHandler
+            public void onEntityInteractEvent(EntityInteractEvent event) {
+                processEntityEvent(event);
+            }
+
+            @EventHandler
+            public void onEntityShootBowEvent(EntityShootBowEvent event) {
+                processEntityEvent(event);
+            }
+        }, this);
+
         new BukkitRunnable() {
             @Override
             public void run() {
-                boolean atLeastOnePlayerMoved = false;
-                for (World world : Bukkit.getServer().getWorlds()) {
-                    for (Entity entity : world.getEntities()) {
-                        if (entity instanceof Player) {
-                            Player player = (Player) entity;
-                            Location previousLocation = previousPlayerLocations.get(player);
-                            if (previousLocation == null) {
-                                previousPlayerLocations.put(player, player.getLocation());
-                            } else {
-                                Location newLocation = player.getLocation();
-                                if (!atLeastOnePlayerMoved && !samePosition(previousLocation, newLocation)) {
-                                    atLeastOnePlayerMoved = true;
-                                }
-                                previousPlayerLocations.put(player, newLocation);
-                            }
-                        }
-                    }
-                }
                 for (World world : Bukkit.getServer().getWorlds()) {
                     for (Entity entity : world.getEntities()) {
                         if (entity instanceof HumanEntity) {
                             continue;
                         }
-                        if (atLeastOnePlayerMoved || !spawnLocations.containsKey(entity)) {
-                            spawnLocations.put(entity, entity.getLocation());
+                        if (atLeastOnePlayerActedThisTick || !entityLocations.containsKey(entity)) {
+                            entityLocations.put(entity, entity.getLocation());
                         } else {
-                            entity.teleport(spawnLocations.get(entity));
+                            entity.teleport(entityLocations.get(entity));
                         }
                     }
                 }
+                atLeastOnePlayerActedLastTick = atLeastOnePlayerActedThisTick;
+                atLeastOnePlayerActedThisTick = false;
             }
         }.runTaskTimer(this, 1, 1);
-	}
+    }
+
+    protected void setPlayerActed() {
+        atLeastOnePlayerActedThisTick = true;
+    }
 
     protected boolean samePosition(Location l1, Location l2) {
         return
                 Double.doubleToLongBits(l1.getX()) == Double.doubleToLongBits(l2.getX()) &&
-                Double.doubleToLongBits(l1.getY()) == Double.doubleToLongBits(l2.getY()) &&
-                Double.doubleToLongBits(l1.getZ()) == Double.doubleToLongBits(l2.getZ());
+                        Double.doubleToLongBits(l1.getY()) == Double.doubleToLongBits(l2.getY()) &&
+                        Double.doubleToLongBits(l1.getZ()) == Double.doubleToLongBits(l2.getZ());
+    }
+
+    protected void processVehicleEvent(VehicleEvent event) {
+        // todo: this one is trickier because we need to cancel vehicle move events associated with
+        // monsters, but not vehicle move events associated with players, and the VehicleMoveEvent
+        // does not contain an entity, so we need to track the entities ourselves
+    }
+
+    protected <T extends EntityEvent & Cancellable> void processEntityEvent(T event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof HumanEntity) {
+            setPlayerActed();
+        } else if (!atLeastOnePlayerActedLastTick) {
+            event.setCancelled(true);
+        }
     }
 }
